e5b182c1fba472f628c2c655351505e890697277
Cindy Qi Li <cli@ocad.ca>
2011-12-01 12:10:20 -0500

    FLUID-4566: Initial check-in of the fluid-studios theme that is modified based on Johnny's FSSFive wordpress theme with UIO implementation.

---
+++ wp-content/themes/fluid-studios/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+		<div id="content-container" class="fl-clearfix fl-container fl-container-flex fl-push">
+
+			<section id="nav:content" class="fl-clearfix fl-col fl-container-flex75" role="main">
+
+				<article id="404">
+					<h1>404 Error &mdash; Page not found</h1>
+					<p>Sorry, but the requested page does not exist or was was found. Please try again or contact this site's administrator's for assistance. Thank you for your interest.</p>
+				</article><!-- /#404 -->
+
+			</section><!-- /#nav:content -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/fluid-studios/README.txt
@@ -0,0 +1,2 @@
+The Fluid Studios theme is based on FSSFive, by Johnny Taylor. The original code was taken from commit cd0b299e911dbb5362b8a40ea6a8feb8cff27cc9 of his github repo: https://github.com/abledaccess/FSSFive
+
