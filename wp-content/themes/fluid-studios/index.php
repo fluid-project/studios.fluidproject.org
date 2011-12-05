543032c4737fc61187dcd215570151b5f6e2d421
Cindy Qi Li <cli@ocad.ca>
2011-12-05 10:36:31 -0500

    FLUID-4566: Format archive page that is to display the posts with the selected tag.

--- wp-content/themes/fluid-studios/index.php
+++ wp-content/themes/fluid-studios/index.php
@@ -4,41 +4,15 @@
 
 <?php get_sidebar(); ?>
 
-            <section id="nav:content" class="fl-clearfix fl-col-main" role="main">
+			<section id="nav:content" class="fl-clearfix fl-col-main" role="main">
 
-                <ul class="fl-grid">
-    				<?php 
-    				if (have_posts()) : 
-    					while (have_posts()) : 
-    						the_post();
-    				?>
-    
-                    <li>
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                            <header>
-                                <div class="fs-post-thumbnail">
-                                    <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php 
-                                    if ( has_post_thumbnail() ) {
-                                        // the current post has a thumbnail
-                                        the_post_thumbnail();
-                                    } else {
-                                        // the current post lacks a thumbnail, display the default picture
-                                        echo '<img alt="Featured image is missing" src="' . get_template_directory_uri() .'/images/placeholder.jpg" width="' . THUMBNAIL_WIDTH . '" />';
-                                    }
-                                    ?></a>
-                                </div>
-                                <h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Direct Link to <?php the_title_attribute(); ?>"><?php echo the_title('', '', false); ?></a></h2>
-                            </header>
-                            <section class="entry-content">
-                                <?php echo the_excerpt(); ?>
-                            </section><!-- /.entry-content -->
-                            <footer class="entry-utility">
-                                <div class="fs-tags"><?php the_tags("", ", "); ?></div> 
-                            </footer><!-- /.entry-utility -->
-                        </article><!-- /#post-<?php the_ID(); ?> -->
-                    </li>
-    				<?php endwhile; ?>
-                </ul>
+				<ul class="fl-grid">
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<li><?php include (TEMPLATEPATH . '/inc/single-post-summary.php' ); ?></li>
+
+					<?php endwhile; ?>
+				</ul>
 
 				<?php 
 				$next_posts = get_next_posts_link('&laquo; Older articles');
@@ -61,6 +35,6 @@
 
 			</section><!-- /#nav:content -->
 
-        </div><!-- /#content-container -->
+		</div><!-- /#content-container -->
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/fluid-studios/sidebar-non-index.php
@@ -0,0 +1,7 @@
+			<aside id="sidebar" class="fl-clearfix fl-col fl-container-flex25" role="complementary">
+				<?php include (TEMPLATEPATH . '/inc/sidebar-common.php' ); ?>
+
+				<div class="fs-back-link">
+					<a href="javascript: void();" onclick="javascript: window.history.back();" title="Go Back">Back</a>
+				</div>
+			</aside><!-- /#sidebar -->
