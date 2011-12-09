88ef7c1cca5824b7034b95bc6eb67601c6fd88f7
Anastasia Cheetham <ac@anastasiacheetham.ca>
2011-12-09 13:28:53 -0500

    STUDIO-18: Fixes to the "404" page to make it look more like the rest of the site.

--- wp-content/themes/fluid-studios/404.php
+++ wp-content/themes/fluid-studios/404.php
@@ -1,16 +1,19 @@
 <?php get_header(); ?>
 
-		<div id="content-container" class="fl-clearfix fl-container fl-container-flex fl-push">
+		<div id="content-container" class="fl-clearfix fl-col-mixed2 fl-push">
 
-			<section id="nav:content" class="fl-clearfix fl-col fl-container-flex75" role="main">
+<?php get_sidebar("non-index"); ?>
+
+			<section id="nav:content" class="fl-clearfix fl-col-main" role="main">
 
 				<article id="404">
-					<h1>404 Error &mdash; Page not found</h1>
+					<h1>Page not found</h1>
 					<p>Sorry, but the requested page does not exist or was was found. Please try again or contact this site's administrator's for assistance. Thank you for your interest.</p>
 				</article><!-- /#404 -->
 
 			</section><!-- /#nav:content -->
 
-<?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
+
+		</div><!-- /#content-container -->
+		
