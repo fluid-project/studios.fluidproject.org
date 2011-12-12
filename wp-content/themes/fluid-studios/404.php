b86549a5ee96865031207bccb1fee1cf2138cac2
Anastasia Cheetham <ac@anastasiacheetham.ca>
2011-12-12 12:50:36 -0500

    STUDIO-18: Updating styles to match Joanna's review changes.

--- wp-content/themes/fluid-studios/404.php
+++ wp-content/themes/fluid-studios/404.php
@@ -6,7 +6,7 @@
 
 			<section id="nav:content" class="fl-clearfix fl-col-main" role="main">
 
-				<article id="404">
+				<article id="404" class="fl-centered">
 					<h1>Page not found</h1>
 					<p>Sorry, but the requested page does not exist or was was found. Please try again or contact this site's administrator's for assistance. Thank you for your interest.</p>
 				</article><!-- /#404 -->
