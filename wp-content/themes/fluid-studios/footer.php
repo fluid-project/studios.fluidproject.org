ce558e7308a248a5bac3f607fe83d1011b4275c2
Anastasia Cheetham <ac@anastasiacheetham.ca>
2011-12-12 15:29:22 -0500

    STUDIO-29: Fix the copyright text and 'top' link.

--- wp-content/themes/fluid-studios/footer.php
+++ wp-content/themes/fluid-studios/footer.php
@@ -3,8 +3,8 @@
 	<footer class="footer" role="contentinfo">
 		<section id="colophon">
 			<ul>
-				<li>&copy; <?= date('Y'); ?> <?php bloginfo('name'); ?></li>
-				<li><a href="#nav:page-top" title="Return to the TOP of this page">TOP</a></li>
+				<li>&copy; <?= date('Y'); ?> OCAD University</li>
+				<li><a href="#nav:page-top" title="Return to the top of this page">Top</a></li>
 			</ul>
 		</section><!-- /#colophon -->
 
