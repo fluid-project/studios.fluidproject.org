746ee2fcc8b53d2f9e5aaf91069187bc86bcc821
Michelle D'Souza <michelled33@gmail.com>
2011-12-14 12:57:51 -0500

    STUDIO-17: Adding in styling for content paragraphs.

--- wp-content/themes/fluid-studios/footer.php
+++ wp-content/themes/fluid-studios/footer.php
@@ -1,7 +1,7 @@
 
 	
 	<footer class="footer" role="contentinfo">
-        &copy; <?= date('Y'); ?> OCAD University <a href="#nav:page-top" title="Return to the top of this page">Top</a>
+        &copy; <?= date('Y'); ?> OCAD University — <a href="#nav:page-top" title="Return to the top of this page">Top</a>
 		
 		<!-- Required for wordpress to put in the admin bar at the top! -->	
 	    <?php wp_footer(); ?>
