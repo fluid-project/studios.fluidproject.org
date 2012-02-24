7efee150e58ed21f0d6d56ba970297dd8a66a27b
Johnny Taylor <johnny@abledaccess.com>
2012-02-23 19:18:39 -0500

    STUDIO-45: All finished, I hope.

--- wp-content/themes/fluid-studios/404.php
+++ wp-content/themes/fluid-studios/404.php
@@ -4,14 +4,14 @@
 		<!-- This is where the Table of Contents will be displayed -->
 		<div class="flc-toc-tocContainer toc"></div><!-- /.flc-tocContainer -->
 
-				<article id="404" class="fl-centered">
-					<h1>Page not found</h1>
-					<p>Sorry, but the requested page does not exist or was not found. Please try again or contact the
-						<a href="mailto:fluid-talk@fluidproject.org">Fluid Studios discussion email list</a>
-						for assistance. Thank you for your interest.</p>
-				</article><!-- /#404 -->
+			<article id="404" class="fl-centered">
+				<h1 class="entry-title">Page not found</h1>
+				<section class="entry-content">
+					<p>Sorry, but the requested page does not exist or was not found. Please try again or contact the <a href="mailto:fluid-talk@fluidproject.org">Fluid Studios discussion email list</a> for assistance. Thank you for your interest.</p>
+				</section><!-- /.entry-content -->
+			</article><!-- /#404 -->
 
-			</section><!-- /#nav:content -->
+		</section><!-- /#nav:content -->
 
 <?php get_footer(); ?>
 		
