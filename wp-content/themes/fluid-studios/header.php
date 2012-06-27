58aab65397abeed34b93190aba1828c1c757be70
Johnny Taylor <johnny@abledaccess.com>
2012-06-27 12:05:10 -0400

    STUDIO-53: Removed an !important from the login button that appeared to be doing nothing in the IE8 styles and resolved a slight shift in that same button on the archive pages also in IE8.

--- wp-content/themes/fluid-studios/header.php
+++ wp-content/themes/fluid-studios/header.php
@@ -8,15 +8,7 @@
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 
-<?php if(is_home() && (!$paged || $paged == 1) || is_single() || is_page()) { ?>
-<meta name="googlebot" content="index,archive,follow,noodp" />
-<meta name="robots" content="all,index,follow" />
-<meta name="msnbot" content="all,index,follow" />
-<?php } else { ?>
-<meta name="googlebot" content="noindex,noarchive,follow,noodp" />
-<meta name="robots" content="noindex,follow" />
-<meta name="msnbot" content="noindex,follow" />
-<?php } ?>
+<title><?php if (function_exists('is_tag') && is_tag()) { single_tag_title("Entries tagged &quot;"); echo'&quot; &mdash; '; } elseif (is_archive()) { wp_title(''); echo ' Archive &mdash; '; } elseif (is_search()) { echo 'Search for &quot;'.esc_html($s).'&quot; &mdash; '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' &mdash; '; } elseif (is_404()) { echo '404 Error &mdash; Page not found &mdash; '; } if (is_home()) { bloginfo('name'); echo ' &mdash; '; bloginfo('description'); } else { bloginfo('name'); } ?><?php if ($paged>1) { echo ' &mdash; page '. $paged; } ?></title>
 
 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 <?php $template_url = get_bloginfo( 'template_url', 'display' ); ?>
@@ -45,8 +37,6 @@
 <script type="text/javascript" src="<?php echo $template_url; ?>/js/fluid-studios.js"></script>
 <script type="text/javascript" src="<?php echo $template_url; ?>/js/modernizr.js"></script>
 
-<title><?php if (function_exists('is_tag') && is_tag()) { single_tag_title("Entries tagged &quot;"); echo'&quot; &mdash; '; } elseif (is_archive()) { wp_title(''); echo ' Archive &mdash; '; } elseif (is_search()) { echo 'Search for &quot;'.esc_html($s).'&quot; &mdash; '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' &mdash; '; } elseif (is_404()) { echo '404 Error &mdash; Page not found &mdash; '; } if (is_home()) { bloginfo('name'); echo ' &mdash; '; bloginfo('description'); } else { bloginfo('name'); } ?><?php if ($paged>1) { echo ' &mdash; page '. $paged; } ?></title>
-
 <link rel="shortcut icon" href="<?php echo $template_url; ?>/favicon.ico" type="image/x-icon" />
 <link rel="apple-touch-icon" href="<?php echo $template_url; ?>/apple-touch-icon.png"/>
 		
@@ -55,8 +45,7 @@
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS 2.0 Feed" href="<?php bloginfo('rss2_url'); ?>">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-<?php if ( is_singular() && get_option( 'thread_comments' ) )
-	wp_enqueue_script( 'comment-reply' );
+<?php 
 	wp_head(); 
 ?>
 
@@ -112,8 +101,8 @@
 				<<?php echo $heading_tag; ?> class="fls-logo">
 					<a href="<?php echo esc_url(get_home_url()); ?>" title="Fluid Studios Home" class="fl-hidden-replace" rel="home">Fluid Studios</a>
 				</<?php echo $heading_tag; ?>>
-				<nav class="fls-access">
-					<ul class="fl-clearfix">
+				<nav class="fls-access"role="navigation" aria-labelledby="main-menu-navigation">
+					<ul id="main-menu-navigation" class="fl-clearfix">
 						<li id="uioProxyButton" class="fl-toggleButton"><a href="#nav:page-top" title="Show display preferences">Show display preferences</a></li>
 						<li class="fls-loginout-link"><?php wp_loginout(); ?></li>
 						<?php if(is_single() || is_archive()) echo('<li class="fls-back-link"><!-- Note: The id "flsc-go-back-link" is used @ js/fluid-studios.js. Synchronizing both is the key to have "back" link work. -->
